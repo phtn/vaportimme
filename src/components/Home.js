7b83aa7f1b091b923dcad22ca42f416a7f17484a
phtn458 <phtn458@gmail.com>
2018-04-05 21:23:11 -0400

    updated header bg color

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import Store from '../assets/store_medium.jpg'
 import Juice from '../assets/juice_medium.jpg'
 import Equip from '../assets/equip_medium.jpg'
 import Wheel from '../assets/cbd_wheel.jpg'
+import Vaping from '../assets/vaping.PNG'
 import { Grid, Card, Image, Segment, Item } from 'semantic-ui-react'
 // const settings = {
 //   dots: false,
@@ -17,7 +18,7 @@ import { Grid, Card, Image, Segment, Item } from 'semantic-ui-react'
 
 const styles = {
   container: { marginTop: 10},
-  slides: { border: '1px solid red '},
+  segment: { margin: 20 },
   jumbotron: { marginTop: 25}
 }
 export default props => (
@@ -25,7 +26,7 @@ export default props => (
         <Grid padded='horizontally'>
           <Grid.Row columns={3} textAlign='center'>
           <Grid.Column textAlign='center'>
-            <Card color='purple' centered>
+            <Card color='purple' centered className='animated fadeIn cardONe'>
               <Image src={Store} />
               <Card.Content>
                 <Card.Header>Store</Card.Header>
@@ -34,7 +35,7 @@ export default props => (
           </Grid.Column>
 
           <Grid.Column>
-            <Card color='purple' centered>
+            <Card color='purple' centered className='animated fadeIn cardTwo'>
               <Image src={Juice} />
               <Card.Content>
                 <Card.Header>Juice</Card.Header>
@@ -43,7 +44,7 @@ export default props => (
           </Grid.Column>
 
           <Grid.Column>
-            <Card color='purple' centered>
+            <Card color='purple' centered className='animated fadeIn cardThree'>
               <Image src={Equip} />
               <Card.Content>
                 <Card.Header>Equipment</Card.Header>
@@ -61,8 +62,8 @@ export default props => (
       
 
     <div style={styles.jumbotron}>
-      <Segment>
-        <Item.Group>
+      <Segment style={styles.segment}>
+        <Item.Group divided>
   
           <Item>
           <Item.Image size='small' src={Wheel} />
@@ -70,16 +71,17 @@ export default props => (
               <Item.Header as='a'>Introducing the CBD Oil: Uses, Benefits and Risks </Item.Header>
               <Item.Meta>Cannabidiol (CBD) oil is used for health benefits, but it is controversial.</Item.Meta>
               <Item.Description>
+              source: [Leafly]
               </Item.Description>
               <Item.Extra>Read More...</Item.Extra>
             </Item.Content>
           </Item>
 
           <Item>
-          <Item.Image size='small' src={Wheel} />
+          <Item.Image size='small' src={Vaping} />
             <Item.Content>
-              <Item.Header as='a'>Introducing the CBD Oil: Uses, Benefits and Risks </Item.Header>
-              <Item.Meta>Cannabidiol (CBD) oil is used for health benefits, but it is controversial.</Item.Meta>
+              <Item.Header as='a'>Scientists are shocked after testing E-cig Vapor in the Lab</Item.Header>
+              <Item.Meta>Source: [ChurnMag.com].</Item.Meta>
               <Item.Description>
               </Item.Description>
               <Item.Extra>Read More...</Item.Extra>
