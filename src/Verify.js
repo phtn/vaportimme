97aa256dde094d21940d420fdc86675502964a27
phtn458 <phtn458@gmail.com>
2018-04-06 16:45:01 -0400

    added react-router and age verifier

---
+++ src/Verify.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Button, Image, Grid, Message } from 'semantic-ui-react'
+import Logo from './assets/logo.png'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import App from './App'
+const styles = {
+  container: { 
+    height: window.innerHeight,
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex'
+  },
+  logo: {
+    height: 100,
+    float: 'left'
+  }
+}
+
+const renderComponent = component => {
+
+}
+
+const Verifier = props => (
+  <div className='App-verify' style={styles.container}>
+    <Grid padded>
+      <Grid.Row rows={2} centered>
+        <Image src={Logo} style={styles.logo} className='logo'/>   
+      </Grid.Row>
+
+      <Grid.Row centered>
+        <Button positive as='a' href='/home'>I am 18 and over</Button>
+        <Button primary>Get me outta here</Button>
+      </Grid.Row>
+
+      <Grid.Row centered>
+        <Message floating className='animated fadeInUp'>
+          <Message.Header>
+            Notice
+          </Message.Header>
+          <p>
+            You must be 18 years old and above to enter this site.
+          </p>
+        </Message>
+      </Grid.Row>
+    </Grid>
+    
+  </div>
+)
+
+export default props => (
+  <Router>
+    <div>
+      <Route exact path='/' render={Verifier}/>
+      <Route strict path='/home' component={App}/>
+    </div>
+  </Router>
+)
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import VerifyAge from './Verify'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<VerifyAge />, document.getElementById('root'));
 registerServiceWorker();
