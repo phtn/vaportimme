c03e0e93f46104c1af6cffe7762625aa0c80bb7b
phtn458 <phtn458@gmail.com>
2018-04-09 17:24:58 -0400

    put back BrowserRouter

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route, Switch,  } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, browserHistory} from 'react-router-dom'
 // Routes
 import AgeVerifier from './components/AgeVerifier'
 import App from './App'
@@ -10,7 +10,7 @@ import Juice from './components/Juice'
 
 
 export default props => (
-  <Router>
+  <Router history={browserHistory}>
     <Switch>
       <Route exact path='/' component={AgeVerifier}/>
       <Route path='/home' component={App}/>
