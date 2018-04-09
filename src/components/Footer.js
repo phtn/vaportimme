5ca418f23b13d0f67b7e9f4973243cef30cf47e5
phtn458 <phtn458@gmail.com>
2018-04-09 14:57:00 -0400

    updated footer with 2 columns

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,13 @@ import { Grid, Divider } from 'semantic-ui-react'
 import Facebook from '../assets/facebook.svg'
 import Twitter from '../assets/twitter.svg'
 import Instagram from '../assets/instagram.svg'
+import Phone from '../assets/phone.svg'
+import Pin from '../assets/pin.svg'
 
 const styles = {
   container: {
     backgroundColor: '#222',
-    height: 275,
+    height: 'auto',
     textAlign: 'center',
     fontFamily: 'Roboto, sans-serif',
     fontWeight: 100,
@@ -23,7 +25,8 @@ const styles = {
   },
   menuItem: {
     color: '#ccc',
-    lineHeight: 2
+    lineHeight: 2,
+    fontSize: '12px'
   },
   bottomText: {
     lineHeight: 3
@@ -32,33 +35,54 @@ const styles = {
     fontWeight: 400
   },
   icons: {
-      height: 10,
+      height: 20,
       marginRight: 5
+  },
+  pin: {
+    height: 10,
+    marginRight: 5
   }
 }
 export default props => (
   <div style={styles.container}>
     <Grid divided='vertically'>
-        <Grid.Row columns={3}>
-          <Grid.Column>
-            <span style={styles.menuHeader}>Follow Us</span><br/>
-            <a href='https://www.instagram.com/vaportimme/'><span style={styles.menuItem}><img src={Facebook} style={styles.icons}/>Facebook</span></a><br/>
-            <a href='https://www.facebook.com/vaportimmenewhope/'><span style={styles.menuItem}><img src={Instagram} style={styles.icons}/>Instagram</span></a><br/>
-            <a href='https://twitter.com/vaportimme?lang=en'><span style={styles.menuItem}><img src={Twitter} style={styles.icons}/>Twitter</span></a>
-          </Grid.Column>
-          <Grid.Column>
-            <span style={styles.menuHeader}>Contact Us</span><br/>
-            <span style={styles.menuItem}>T 215.693.1905</span><br/>
-            <span style={styles.menuItem}>vaportimme@gmail.com</span>
-          </Grid.Column>
-          <Grid.Column>
-            <span style={styles.menuHeader}>Find Us</span><br/>
-            <span style={styles.menuItem}>15 West Ferry Street</span><br/>
-            <span style={styles.menuItem}>New Hope, PA 18938</span>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    
+      <Grid.Row columns={2}>
+        
+        <Grid.Column>
+          <a href='https://www.facebook.com/vaportimmenewhope/'><span style={styles.menuItem}><img src={Facebook} style={styles.icons} alt=''/></span></a><br/>
+          <span style={styles.menuItem}>Facebook</span>
+        </Grid.Column>
+        <Grid.Column>
+          <a href='https://www.instagram.com/vaportimme/'><span style={styles.menuItem}><img src={Instagram} style={styles.icons} alt=''/></span></a><br/>
+          <span style={styles.menuItem}>Instagram</span>
+        </Grid.Column>
+
+      </Grid.Row>
+
+      <Grid.Row columns={2}>
+      <Grid.Column>
+          <a href='https://twitter.com/vaportimme?lang=en'><span style={styles.menuItem}><img src={Twitter} style={styles.icons} alt=''/></span></a><br/>
+          <span style={styles.menuItem}>Twitter</span>
+        </Grid.Column>
+        <Grid.Column>
+          <a href='tel: 215-693-1905'><span style={styles.menuItem}><img src={Phone} style={styles.icons} alt=''/></span></a><br/>
+          <span style={styles.menuItem}>T 215.693.1905</span>
+          
+        </Grid.Column>
+        
+      </Grid.Row>
+
+      <Grid.Row columns={1}>
+        
+        <Grid.Column>
+        <img src={Pin} style={styles.pin} alt=''/>
+          <span style={styles.menuItem}>15 W Ferry Street New Hope, PA 18938</span>
+        </Grid.Column>
+       
+
+      </Grid.Row>
+
+    </Grid>
     <Divider section/>
     <span style={styles.usageLinks}>Terms of Use</span> &nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;
     <span style={styles.usageLinks}>Privacy Policy</span><br/>
