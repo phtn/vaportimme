5fd4716987d50f2fdb97223da40f6a5224089b73
phtn458 <phtn458@gmail.com>
2018-05-20 06:17:20 -0400

    reverted back using iframe

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,9 @@ import Equip from '../assets/resized/equip_medium.jpg'
 import Wheel from '../assets/cbd_wheel.jpg'
 import Vaping from '../assets/resized/vaping_medium.jpg'
 import NoSmoking from '../assets/nosmoking.svg'
-// import Video from './Video'
-import Vid1 from '../assets/vid1.jpg'
-import Vid2 from '../assets/vid2.jpg'
+import Video from './Video'
+// import Vid1 from '../assets/vid1.jpg'
+// import Vid2 from '../assets/vid2.jpg'
 // import YouTubeSvg from '../assets/cctv.svg'
 
 // import Benefits from './Benefits'
@@ -182,11 +182,12 @@ export default props => (
           <Grid stackable>
           <Grid.Row columns={3}>
           <Grid.Column>
-          <Card as='a' href='https://www.youtube.com/watch?v=Fh-s64RNtz0' fluid >
-            <Image src={Vid1} size='big'style={{maxHeight: 300}}/>
+          <Card fluid >
+            {/* <Image src={Vid1} size='big'style={{maxHeight: 300}}/> */}
             <Card.Content>
-              <Card.Header>Doctors Speak Out on the Benefits of CBD</Card.Header>
-              <Card.Meta><Icon name='youtube' color='red'/>&nbsp;<span style={{color: '#666'}}>YouTube</span></Card.Meta>
+              <Video url={'https://www.youtube.com/embed/Fh-s64RNtz0'}/>
+              {/* <Card.Header>Doctors Speak Out on the Benefits of CBD</Card.Header>
+              <Card.Meta><Icon name='youtube' color='red'/>&nbsp;<span style={{color: '#666'}}>YouTube</span></Card.Meta> */}
               {/* <Card.Description>Daniel is a comedian living in Nashville.</Card.Description> */}
             </Card.Content>
             {/* <Card.Content extra>
@@ -199,22 +200,24 @@ export default props => (
           </Grid.Column>
 
           <Grid.Column>
-          <Card as='a' href='https://www.youtube.com/watch?v=U71YItZTie8' fluid style={{maxHeight: 400}}>
-            <Image src={Vid2} style={{maxHeight: 400}}/>
+          <Card fluid >
+            {/* <Image src={Vid2} style={{maxHeight: 400}}/> */}
             <Card.Content>
-              <Card.Header>Benefits of CBD</Card.Header>
-              <Card.Meta><Icon name='youtube' color='red'/>&nbsp;<span style={{color: '#666'}}>YouTube</span></Card.Meta>
+            <Video url={'https://www.youtube.com/embed/U71YItZTie8'}/>
+              {/* <Card.Header>Benefits of CBD</Card.Header>
+              <Card.Meta><Icon name='youtube' color='red'/>&nbsp;<span style={{color: '#666'}}>YouTube</span></Card.Meta> */}
             </Card.Content>
           
           </Card>
           </Grid.Column>
 
           <Grid.Column>
-          <Card as='a' href='https://gaming.youtube.com/watch?v=N7298pOTXjE' fluid >
+          <Card fluid >
             {/* <Image src={YouTubeSvg} size='medium' style={{Height: 200}} fluid/> */}
+            <Video url={'https://www.youtube.com/embed/N7298pOTXjE'}/>
             <Card.Content>
-              <Card.Header>New Hope Live</Card.Header>
-              <Card.Meta><Icon name='video' color='purple'/>&nbsp;<span style={{color: '#666'}}>Watch it live</span></Card.Meta>
+              {/* <Card.Header>New Hope Live</Card.Header>
+              <Card.Meta><Icon name='video' color='purple'/>&nbsp;<span style={{color: '#666'}}>Watch it live</span></Card.Meta> */}
             </Card.Content>
           
           </Card>
--- src/components/Video.js
+++ src/components/Video.js
@@ -6,7 +6,7 @@ class Video extends Component {
   render(){
     return (
       // <iframe src={this.props.url} width='100%' height={400} title={this.props.title}></iframe>
-      <iframe width="1230" height="396" src={this.props.url} frameBorder="0" allow="autoplay; encrypted-media" title='video' allowFullScreen></iframe>
+      <iframe width="100%" height="auto" src={this.props.url} frameBorder="0" allow="autoplay; encrypted-media" title='video' allowFullScreen></iframe>
       //
 
     )
