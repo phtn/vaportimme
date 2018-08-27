30b4ef5784a15309b36a8e1abe3fca229cca8673
phtn458 <phtn458@gmail.com>
2018-08-27 07:56:24 -0400

    using object tag for videos

--- src/components/Video.js
+++ src/components/Video.js
@@ -14,7 +14,9 @@ class Video extends Component {
       //   style={{marginBottom: 20, width: '100%'}}
       // />
       <div>
-        <embed src={`http://www.youtube.com/v/${this.props.url}?version=3&amp;hl=en_US&amp;rel=0&amp;autohide=1&amp;autoplay=1`} wmode="transparent" type="application/x-shockwave-flash" width="100%" height="100%" allowfullscreen="true" title="Adobe Flash Player"/>
+        {/* <embed src={`http://www.youtube.com/v/${this.props.url}?version=3&amp;hl=en_US&amp;rel=0&amp;autohide=1&amp;autoplay=1`} wmode="transparent" type="application/x-shockwave-flash" width="100%" height="100%" allowfullscreen="true" title="Adobe Flash Player"/> */}
+        <object  data={`http://www.youtube.com/v/${this.props.url}?version=3&amp;hl=en_US&amp;rel=0&amp;autohide=1&amp;autoplay=1`} width="100%" height="100%" allowfullscreen="true">
+</object>
       </div>
     )
   }
